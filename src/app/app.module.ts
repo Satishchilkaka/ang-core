83f45b1f9cf4165c33b6e95efd6aef97f82baf42
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-10-30 20:23:38 -0400

    Move footer component to shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ItemComponent } from './item/item.component';
-import { ItemDetailsComponent } from './item/item-details/item-details.component';
-
-import { FooterComponent } from './features/footer/footer.component';
 import { ContentComponent } from './features/content/content.component';
 import { HeaderModule } from './shared/header/header.module';
+import { FooterModule } from './shared/footer/footer.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-
-    ItemComponent,
-    ItemDetailsComponent,
-
-    FooterComponent,
     ContentComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HeaderModule
+    HeaderModule,
+    FooterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
