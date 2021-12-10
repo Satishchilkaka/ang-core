a350eda19d8681999d33271accebd0214bdfcd51
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-12-09 23:01:21 -0500

    Add chnages component

---
+++ src/app/cycles/on-changes/on-changes.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-on-changes',
+  templateUrl: './on-changes.component.html',
+  styleUrls: ['./on-changes.component.css']
+})
+export class OnChangesComponent implements OnChanges, OnInit {
+
+  constructor() { }
+
+  @Input() newData: any;
+  onChanges() {
+    this.newData + 1;;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+
+  }
+  ngOnInit(): void {
+
+  }
+
+}
