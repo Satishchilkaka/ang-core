3bc8dcb659a091cf3b2b0d7ed48fd4d49e77c015
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-12-09 23:01:51 -0500

    Add on chnage

--- src/app/cycles/cycles.component.ts
+++ src/app/cycles/cycles.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-cycles',
   templateUrl: './cycles.component.html',
   styleUrls: ['./cycles.component.css']
 })
-export class CyclesComponent implements OnInit {
-
+export class CyclesComponent implements OnInit, OnChanges {
+  data = 0;
   constructor() { }
-
+  changeData() {
+    this.data + 1;
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes)
+  }
   ngOnInit(): void {
   }
-
 }
