ad301fb1b07922a1dc8ea7bc5bb27afc72b8b99b
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2022-04-09 21:10:26 -0400

    add sample subscribe

--- src/app/features/chkbinding/chkbinding.component.ts
+++ src/app/features/chkbinding/chkbinding.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-chkbinding',
@@ -6,13 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chkbinding.component.css']
 })
 export class ChkbindingComponent implements OnInit {
+  example$ = of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
   registration: any;
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
   getRegData() {
     console.log('test')
+    this.example$.subscribe((t: any) => {
+      console.log(t)
+    })
   }
+
+
 }
